633c0e6e0bcdfae300a2db8f7007ea9d8c95a256
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-03-02 14:45:05 +0300

    update script

--- filter_keys.py
+++ filter_keys.py
@@ -2,25 +2,30 @@ import openpyxl
 from openpyxl.styles import Font
 
 # see examples on https://linuxhint.com/manipulating_excel_python/
-
 # Initializing a Workbook
-work_book = openpyxl.load_workbook('search_competitor_ads__2021_02_18 -4.xlsx')
+work_book = openpyxl.load_workbook('words_with_dublicate.xlsx')
 
 print(work_book.sheetnames)
 
 # Navigating to Second Sheet (at index 1)
-work_book.active = 1
+work_book.active = 0
 # Getting Active Sheet
 sheet = work_book.active
 print(sheet.title)
-# Adding Data to ‘A1’ Cell of Second Sheet
-#sheet['A1'] = 'ID'
-print(sheet['A1'].value)
+# 1 Удаляем все дубликаты - проходим по ключам с конца, и перезаписываем повторение.
+sheet_values = tuple(list(sheet.values)[::-1])
+result = {sheet_values[i][0]: (sheet_values[i][1], sheet_values[i][2])
+          for i in range(len(sheet_values))}
+print(result)
+# Инициализация новой книги
+work_book = openpyxl.Workbook()
+work_book.create_sheet(index=0, title='Words')
+work_book.active = 0
+# Getting Active Sheet
+sheet = work_book.active
+for n, (key, values) in enumerate(result.items(), 1):
+    sheet[f'A{n}'] = key
+    sheet[f'B{n}'] = values[0]
+    sheet[f'C{n}'] = values[1]
 
-# Creating style object
-style = Font(name='Consolas', size=13, bold=True, italic=False)
-a1 = sheet['A1']
-a1.font = style
-# saving workbook as ‘example.xlsx’
-
-work_book.save('example.xlsx')
+work_book.save('words_for_direct.xlsx')
